c50882ca7dcd2da5e40265014746fc7cb0815446
gregor-J8 <gregor.johnson@outlook.com>
2025-03-31 16:47:36 -0500

    added types to like service

--- src/services/likeService.ts
+++ src/services/likeService.ts
@@ -2,11 +2,11 @@ export const getLikes = () => {
     return fetch(`http://localhost:8088/userLikes`).then((res) => res.json())
   }
   
-export const GetFavoritePosts = (userId) => {
+export const GetFavoritePosts = (userId: number) => {
     return fetch(`http://localhost:8088/userLikes?userId=${userId}&isLiked=true&_expand=user&_expand=post`).then((res) => res.json())
   }
   
-  export const createUserLike = async(user) => {
+  export const createUserLike = async(user: User) => {
     return fetch(`http://localhost:8088/userLikes`, {
       method: "POST",
       headers: {
@@ -16,7 +16,7 @@ export const GetFavoritePosts = (userId) => {
     }).then((res) => res.json())
   }
 
-  export const deleteUserLike = (id) => {
+  export const deleteUserLike = (id: number) => {
     return fetch(`http://localhost:8088/userLikes/${id}`, {
       method: "DELETE",
     })};
