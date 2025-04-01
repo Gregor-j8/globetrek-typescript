fe9416d3918e7a3ff7139c8bc192ba60f3a4115c
gregor-J8 <gregor.johnson@outlook.com>
2025-04-01 07:10:51 -0500

    added types to NewPostContext

--- src/context/NewPostContext.tsx
+++ src/context/NewPostContext.tsx
@@ -1,15 +1,20 @@
-import React, { createContext, useState, useContext } from 'react';
+import { createContext, useState, useContext } from 'react'
 
-const PostContext = createContext()
+interface PostContextType {
+  newPost: Post,
+  updatePost:(updatedPost: Partial<Post>) => void;
+}
+
+const PostContext = createContext<PostContextType | undefined>(undefined)
 
 export const NewPostContext = () => {
   return useContext(PostContext)
 }
 
-export const NewPostHolder = ({ children }) => {
-  const [newPost, setNewPost] = useState({})
+export const NewPostHolder: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [newPost, setNewPost] = useState<Post>({} as Post)
 
-  const updatePost = (updatedPost) => {
+  const updatePost = (updatedPost: Partial<Post>) => {
     setNewPost((prevPost) => ({ ...prevPost, ...updatedPost }))
   }
 
